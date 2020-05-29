fc2e787252f46bef518d90fb3fe677fa2d683e94
joaobitencourt <jobitencourt924@gmail.com>
2020-05-29 12:44:09 -0300

    fixação
    commit

---
+++ Fixacao2/app/src/main/java/com/example/fixacao/EnviarEmail.java
@@ -0,0 +1,14 @@
+package com.example.fixacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnviarEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviar_email);
+    }
+}
